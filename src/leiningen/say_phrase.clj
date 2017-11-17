62d1a44f4493f6f58967d67c087d2190a66d3fc0
Bansari <bansari.ds30@gmail.com>
2017-11-17 16:14:17 -0500

    adding wish phrase

--- src/leiningen/say_phrase.clj
+++ src/leiningen/say_phrase.clj
@@ -3,4 +3,7 @@
 (defn say-phrase
   "I don't do a lot."
   [project & args]
-  (println "Hi!"))
+  (condp = (first args)
+    "morning" (leiningen.core.main/info "Good Morning!")
+    "evening" (leiningen.core.main/info "Good Evening!")
+    (leiningen.core.main/info "Hello World!")))
